0864c5501d7c25f78f2c20a29e1925e2bf873978
Sachin Sangade <sacsangade@gmail.com>
2025-07-04 02:23:02 +0530

    second commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL);
+const url = process.env.DATABASE_URL;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log("Server up and running!")
+.catch((error) => console.log(error.message); 
+mongoose.set('useFindAndModify', false);
 var conn = mongoose.connection;
 conn.on('connected', function() {
     console.log('database is connected successfully');
