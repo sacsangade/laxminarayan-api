f039aa040fde137949ad79c21a00a62c4139808c
Sachin Sangade <sacsangade@gmail.com>
2025-07-04 02:27:55 +0530

    third commit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 const url = process.env.DATABASE_URL;
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(PORT, () => console.log("Server up and running!")
-.catch((error) => console.log(error.message); 
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 var conn = mongoose.connection;
 conn.on('connected', function() {
