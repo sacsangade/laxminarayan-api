a476e69c7bf269949970d370fa052de0bce8a345
Sachin Sangade <sacsangade@gmail.com>
2025-07-04 02:35:33 +0530

    five commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+console.log(process.env.DATABASE_URL);
 const url = process.env.DATABASE_URL;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 var conn = mongoose.connection;
